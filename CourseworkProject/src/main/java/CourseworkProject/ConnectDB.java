2132aeb9b4109bcce453cb1139427b438d11af77
lucas <lucas@DESKTOP-69327SR>
2022-04-15 01:46:06 +0100

    refactored the connectedDB class to > ConnectDB

---
+++ CourseworkProject/src/main/java/CourseworkProject/ConnectDB.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CourseworkProject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alwin
+ */
+public class ConnectDB {
+    
+      public static Connection getConnection() {
+
+        try {
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+//            String url = "jdbc:mysql://phpmyadmin.ecs.westminster.ac.uk:3306/w1783737_0?zeroDateTimeBehavior=convertToNull";
+            String url = "jdbc:mysql://sql4.freesqldatabase.com:3306/sql4483459?zeroDateTimeBehavior=convertToNull";
+            Connection con = DriverManager.getConnection(url);
+
+            JOptionPane.showMessageDialog(null, "Connection Established");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+     
+    
+}
